589d1c0c887b645eb9455526f067661663af2311
Murphy <d2014zjt@163.com>
2019-07-21 22:01:18 +0800

    【修复】原有 SDK 中指针类型强转造成数据出错和非对齐访问的问题
    Signed-off-by: Murphy <d2014zjt@163.com>

--- iotkit-embedded/src/mqtt/impl/iotx_mqtt_client.c
+++ iotkit-embedded/src/mqtt/impl/iotx_mqtt_client.c
@@ -1946,6 +1946,7 @@ static int MQTTSubscribe(iotx_mc_client_t *c, const char *topicFilter, iotx_mqtt
                          iotx_mqtt_event_handle_func_fpt messageHandler, void *pcontext)
 {
     int                         len = 0;
+    int                         qos_sub = (int)qos;
     iotx_time_t                 timer;
     MQTTString                  topic = MQTTString_initializer;
     /*iotx_mc_topic_handle_t handler = {topicFilter, {messageHandler, pcontext}};*/
@@ -2125,7 +2126,7 @@ static int MQTTSubscribe(iotx_mc_client_t *c, const char *topicFilter, iotx_mqtt
     }
 
     len = MQTTSerialize_subscribe((unsigned char *)c->buf_send, c->buf_size_send, 0, (unsigned short)msgId, 1, &topic,
-                                  (int *)&qos);
+                                  (int *)&qos_sub);
     if (len <= 0) {
 #ifdef PLATFORM_HAS_DYNMEM
         mqtt_free(handler->topic_filter);
